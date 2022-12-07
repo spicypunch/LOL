f84517273c728ff0809b44c7bda2c5b9780b8bec
minig <miniguni66@naver.com>
2022-12-07 19:02:07 +0900

    test

---
+++ app/src/main/java/com/example/lol/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.lol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lol.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: Adapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getItem()
+    }
+
+    private fun getItem() {
+        Thread {
+            runOnUiThread {
+
+            }
+        }.start()
+    }
+}
