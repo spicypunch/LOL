54704e32e3fd7cf4f733910fb82020a506a8a2c4
spicypunch <miniguni66@naver.com>
2023-02-24 02:41:06 +0900

    주석 제거

--- app/src/main/java/com/example/lol/MainActivity.kt
+++ app/src/main/java/com/example/lol/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: RecyclerViewAdapter
-    private var List = arrayListOf<LOLResponseItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,22 +26,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.recyclerView
         getItem()
-//        List.add(LOLResponseItem(1, 2, 3, 4, 5,  6, true, 7, "종마루"))
-//        setUI(List)
     }
 
     private fun getItem() {
         val retrofitAPI = RetrofitConnection.getInstance().create(LOLService::class.java)
-        /*excute() 함수는 요청을 현재 쓰레드에서 처리하지만,
-        enqueue() 함수는 백그라운드 스레드에서 요청을 수행한 후에 콜백은 현재 쓰레드에서 처리함 */
         retrofitAPI.getInformation(
-            "RGAPI-ddbd4ed6-a076-4bfe-80ad-10c5f9fa615a"
+            "RGAPI-6aed2e09-5f2f-4521-8171-5a5790c0cfe6"
         ).enqueue(object : Callback<List<LOLResponseItem>> {
             override fun onResponse(
                 call: Call<List<LOLResponseItem>>,
                 response: Response<List<LOLResponseItem>>
             ) {
-                Log.e("response 잘 나오나?", "여기까지 호출 됨 $response")
                 if (response.isSuccessful) {
                     Toast.makeText(this@MainActivity, "데이터 가져오기 성공!", Toast.LENGTH_SHORT).show()
                     response.body()?.let { setUI(it as ArrayList<LOLResponseItem>) }
@@ -52,14 +46,12 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<List<LOLResponseItem>>, t: Throwable) {
-                Log.e("가져오기 실패", "실패?")
                 t.printStackTrace()
             }
         })
     }
 
     private fun setUI(lolData: ArrayList<LOLResponseItem>) {
-        Log.e("loldata", lolData.toString())
         Thread {
             runOnUiThread {
                 adapter = RecyclerViewAdapter(lolData)
@@ -67,11 +59,6 @@ class MainActivity : AppCompatActivity() {
                 binding.recyclerView.layoutManager = LinearLayoutManager(this)
             }
         }.start()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-//        getItem()
     }
 }
 
